01d58f35cb3386f3bbce533e36347db9522b8c6e
shahara21-meet <52987673+shahara21-meet@users.noreply.github.com>
2019-11-24 19:43:35 +0200

    Add files via upload

--- fb_project.py
+++ fb_project.py
@@ -1,3 +1,4 @@
+emails=["broman@gmail.com","themememan@tea.com"]
 class User:
 	def __init__(self,name,email,password,):
 		self.name=name
@@ -5,14 +6,21 @@ class User:
 		self.password=password
 		self.friend_list=[]
 		self.posts=[]
+		emails.append(email)
 
 	def add_friend(self,email):
-		self.friend_list.append(email)
-		print(self.name+" has added "+email+" as a friend")
+		if email in emails:
+			self.friend_list.append(email)
+			print(self.name+" has added "+email+" as a friend")
+		else:
+			print("there is no such user")
 
 	def remove_friend(self,email):
-		self.friend_list.pop(email)
-		print(self.name+" has removed "+email+" from is friend list")
+		if email in friend_list:
+			self.friend_list.pop(email)
+			print(self.name+" has removed "+email+" from is friend list")
+		else:
+			print("you have no such friend")
 
 	def post(self,text):
 		self.posts.append(text)
@@ -33,17 +41,79 @@ class User:
 					print("exiting")
 		else:
 			print("you dont have a friend with this email address")
-	
+
+barak=User("barak","broman@gmail.com","weirdisgood132")
+shahar=User("shahar","themememan@tea.com","iliketea5884")
+
+user_dict={"shahar":"iliketea5884","barak":"weirdisgood132"}
+users={'iliketea5884':shahar,"weirdisgood132":barak}
+
+
+def create_user():
+	x=input("how do you want to call your user? ")
+	y=input("what email do you want to use for your email? ")
+	z=input("write your desired password ")
+	if z in user_dict:
+		z=input("choose another password ")
+
+	user_dict[x]=z
+	username=User(x,y,z)
+	users[z]=username
+
+def login():
+	l_name=input("write username ")
+	l_password=input("write password ")
+	if user_dict[l_name]==l_password:
+		current_user=users[l_password]
+		print("you've logged in!!")
+		return current_user
+	else:
+		print("username or password incorrect")
+
 
 
 
 			
 
 
+print("login/create new user ")
 
 
-user1=User("Barak","broman@gmail.com","weirdisgood132")
-user2=User("Shahar","themememan@tea.com","iliketea5884")
 
-user2.add_friend('broman@gmail.com')
-user2.msg_user("broman@gmail.com")
+
+while True:
+	check=input()
+	if check=="msg "+shahar.name:
+		current_user.msg_user(bhahar.email)
+	elif check=="msg "+barak.name:
+		current_user.msg_user(barak.email)
+	elif check=="create new user":
+		create_user()
+	elif check=="login":
+		current_user=login()
+	elif check=="add friend":
+		n_friend=input("what is his email? ")
+		current_user.add_friend(n_friend)
+	elif check=="remove friend":
+		n_friend=input("what is his email? ")
+		current_user.remove_friend(n_friend)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
